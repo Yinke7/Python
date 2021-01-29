e9f8df9629f1f40ef288ec88a9dac08263224ef6
Yinke7 <1583827745@qq.com>
2021-01-29 17:18:59 +0800

    update

--- Serial/SerialSend.py
+++ Serial/SerialSend.py
@@ -8,11 +8,11 @@ def Run(Serialport, senddata, timeout):
 
     # 设置串口端口号，波特率 115200bps，超时时间 0.1s
     ser = serial.Serial(port=Serialport, baudrate=115200, timeout=float(timeout))
-    print("waitting for %f s" % float(timeout))
+    print("waitting for [%f] s" % float(timeout))
 
     # 发送数据
     ser.write(bytes.fromhex(senddata))
-    print("send 0x%s" % senddata)
+    print("send [%s]" % senddata)
 
     # 接收数据并打印
     try:
@@ -35,3 +35,6 @@ if __name__ == "__main__":
         exit()
 
     Run(sys.argv[1], sys.argv[2], sys.argv[3])
+
+
+
